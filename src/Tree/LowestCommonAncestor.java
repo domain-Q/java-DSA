b1026eb7dac816b33de8515116f407b60effe543
vishal thakur <vishubrahman999@gmail.com>
2023-09-02 12:16:49 +0530

    revesion

--- src/Tree/LowestCommonAncestor.java
+++ src/Tree/LowestCommonAncestor.java
@@ -21,19 +21,18 @@ public class LowestCommonAncestor {
             return null;
         }
 
-        if (root == p || root == q) {
+        if (root == p || root == q) {  // agar root hi p and q doono ho toh aise me root hi hoga LCA ;
             return root;
         }
 
         TreeNode left = LCA(root.left, p, q);
         TreeNode right = LCA(root.right, p, q);
-        if (left != null && right != null) {
+        if (left != null && right != null) { // agar dono me p and q hai to aise me hume ye karna hai ki root return karna hai;
             return root;
-        } else if (left == null) {
+        } else if (left == null) {          // agar left null hai iska matlab ki right me ek address return karna hai so return right;
             return right;
         } else {
-            return left;
+            return left;                    // agar right null hai to iska matlab hai ki left me address return karna hai aur isiliye return left;
         }
-
     }
 }
