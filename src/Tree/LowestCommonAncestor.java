c370cc02c1415dbdaf9f85d6db8f61ad00af1b4c
vishal thakur <vishubrahman999@gmail.com>
2023-09-02 12:11:20 +0530

    revesion

---
+++ src/Tree/LowestCommonAncestor.java
@@ -0,0 +1,39 @@
+package Tree;
+
+public class LowestCommonAncestor {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+    }
+
+    class Solution {
+
+    }
+
+    public TreeNode LCA(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = LCA(root.left, p, q);
+        TreeNode right = LCA(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left == null) {
+            return right;
+        } else {
+            return left;
+        }
+
+    }
+}
